9981ed60115fa7d3993c782019d258a64dfd6c45
RodoAlbornoz <ralbornozt@fi.uba.ar>
2025-07-11 03:11:49 -0300

    Added nodes and links for each degree

---
+++ src/app/models/musicTherapyCorrelations.model.ts
@@ -0,0 +1,18 @@
+import { SubjectsCorrelations } from "./subjectsCorrelations.model";
+
+export class musicTherapyCorrelations implements SubjectsCorrelations {  
+	cbcCorrelations: { materia: string, correlativas: string[] | null }[] = [];
+	cicloGeneralCorrelations: { materia: string, correlativas: string[] | null }[] = [];
+	requisitoIdioma: { materia: string, correlativas: string[] | null }[] = [];
+	cicloProfesionalCorrelations: { materia: string, correlativas: string[] | null}[] = [];
+
+	mapCorrelationsToLinks() {
+		return this.cicloGeneralCorrelations
+		.flatMap(correlations =>
+			correlations.correlativas?.map(correlativa => ({
+				source: correlativa,
+				target: correlations.materia
+			})) ?? []
+		);
+	}
+}
---
+++ src/app/models/musicTherapySubjects.model.ts
@@ -0,0 +1,15 @@
+import { SubjectsDegree } from "./subjectsDegree.model";
+
+export class musicTherapySubjects implements SubjectsDegree {  
+    cbcSubjects: { materia: string, cargaHorariaSemanal: number }[] = [];
+    cicloGeneralSubjects: { materia: string, cargaHorariaSemanal: number }[] = [];
+    requisitoIdioma: { materia: string, duracion: string, cargaHorariaSemanal: number }[] = [];
+    cicloProfesionalSubjects: { materia: string, duracion: string, cargaHorariaSemanal: number }[] = [];
+
+    mapSubjectsToNodes() {
+        return [
+            ...this.cicloGeneralSubjects.map(subject => ({ id: subject.materia, label: subject.materia })), 
+            ...this.cbcSubjects.map(subject => ({ id: subject.materia, label: subject.materia }))
+        ]
+    }
+}
